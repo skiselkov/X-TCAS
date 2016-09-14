486a6dbdf18cf304d00403ba6f580e104e882368
Saso Kiselkov <skiselkov@gmail.com>
2016-09-14 14:59:51 +0200

    Added X-Plane SDK. Added 2 male voice versions.

---
+++ xplane.c
@@ -0,0 +1,15 @@
+#include "XPLMDataAccess.h"
+#include "xplane.h"
+
+void
+xtcas_sim_intf_init(void)
+{
+}
+
+void
+xtcas_sim_intf_fini(void)
+{
+}
+
+void xtcas_get_aircraft_pos(double *lat, double *lon, double *track_true,
+    double *alt, double *alt_agl, double *vvel);
